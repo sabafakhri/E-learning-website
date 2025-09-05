54e19f0accae2b475844d213e119de5d461d4cd5
mehdi95zeid <mehdi95zeid@gmail.com>
2025-09-05 21:39:56 +0330

    refactor(globals.css): update typography styles

--- components/Subscribe.tsx
+++ components/Subscribe.tsx
@@ -4,22 +4,26 @@ import { Input } from "@/components/ui/input";
 export default function Subscribe() {
   return (
     <div className="text-background dark:text-foreground font-metropolis flex flex-col items-center justify-center gap-3.75 bg-gray-900 text-center max-sm:px-5 max-sm:py-10 sm:h-105 sm:gap-5">
-      <h2 className="text-45 font-bold max-sm:hidden">
+      <h2 className="text-45-55 font-bold max-sm:hidden">
         Subscribe For Get Update Every New Courses
       </h2>
-      <h2 className="text-28 font-bold sm:hidden">
+      <h2 className="text-28-32 sm:text-35-40 font-bold sm:hidden">
         Subscribe For Get Update Every New Courses
       </h2>
       <div>
-        <p className="text-16 text-background/70 dark:text-foreground/70 mb-7.5 font-normal sm:mb-10">
+        <p className="text-16-24 lg:text-14-28 text-background/70 dark:text-foreground/70 mb-7.5 font-normal sm:mb-10">
           20k+ students daily learn with Eduvi. Subscribe for new courses.
         </p>
-        <div className="sm:focus-within:ring-ring/50 sm:focus-within:ring-offset-background flex transition-all duration-300 max-sm:flex-col max-sm:gap-5 sm:rounded-[10px] sm:focus-within:ring-[3px]">
+        <div className="sm:focus-within:ring-ring/50 sm:focus-within:ring-offset-background mx-2.5 flex transition-all duration-300 max-sm:flex-col max-sm:gap-5 sm:focus-within:ring-[3px] lg:rounded-[10px]">
           <Input
-            className="peer text-16 placeholder:text-background dark:placeholder:text-foreground h-12.5 rounded-[5px] border-none bg-white/20 font-medium sm:h-15 sm:rounded-[10px] sm:rounded-r-none sm:focus-visible:border-none sm:focus-visible:ring-0"
+            className="peer text-14-30 lg:text-16-30 placeholder:text-background dark:placeholder:text-foreground h-12.5 rounded-[5px] border-none bg-white/20 font-medium sm:rounded-r-none sm:focus-visible:border-none sm:focus-visible:ring-0 lg:h-15 lg:rounded-[10px]"
             placeholder="enter your email"
           ></Input>
-          <Button size="subscribe" variant="rightsideborder">
+          <Button
+            size="subscribe"
+            variant="subscribe"
+            className="lg:font-inter text-14-30 lg:text-16-30"
+          >
             Subscribe
           </Button>
         </div>
